93a5b44ee8d180c6f8e6cc39754e57384dd9e12e
vishnuga <vishnukumaran257@gmail.com>
2022-06-21 11:07:13 +0530

    first commit

---
+++ utils/data.js
@@ -0,0 +1,61 @@
+const data={
+    products: [
+        {
+            name:'Free-shirt',
+            slug: 'Free-shirt',
+            category:'shirt',
+            image:'/images/shirt1.jpg',
+            price:70,
+            brand:'nike',
+            countInStock:20,
+            description:'A popular shirt',
+
+        },
+        {
+            name:'Fit-shirt',
+            slug: 'Fit-shirt',
+            category:'shirt',
+            image:'/images/shirt2.jpg',
+            price:80,
+            brand:'adiddas',
+            countInStock:20,
+            description:'A popular shirt',
+
+        },
+        {
+            name:'model-shirt',
+            slug: 'model-shirt',
+            category:'shirt',
+            image:'/images/shirt3.jpg',
+            price:90,
+            brand:'milan',
+            countInStock:20,
+            description:'A popular pant',
+
+        },
+        {
+            name:'Free-pant',
+            slug: 'Free-pant',
+            category:'pant',
+            image:'/images/pants3.jpg',
+            price:60,
+            brand:'milan',
+            countInStock:20,
+            description:'A popular pant',
+
+        },
+        {
+            name:'model-pant',
+            slug: 'model-pant',
+            category:'pant',
+            image:'/images/pants2.jpg',
+            price:80,
+            brand:'milan',
+            countInStock:20,
+            description:'A popular pant',
+
+        }
+    ]
+}
+
+export default data;
